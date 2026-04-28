736b87c2c82f3f1140c12d095bdefd352ecf9974
kscscafe <sugizaki502@yahoo.co.jp>
2026-04-28 20:43:56 +0900

    Initial Commit

---
+++ hanyuai/hanyuaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  hanyuaiApp.swift
+//  hanyuai
+//
+//  Created by 杉崎康隆 on 2026/04/28.
+//
+
+import SwiftUI
+
+@main
+struct hanyuaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
